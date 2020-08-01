a1436fc9abd73bcbbce2d12132b4ff4aeb4c3c0e
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-08-01 21:50:25 +0300

    add experts component

---
+++ src/app/components/experts/experts.component.ts
@@ -0,0 +1,29 @@
+import {Router} from '@angular/router';
+import { ExpertService } from './../../shared/services/expert/expert.service';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Expert } from './../../shared/models/expert';
+
+@Component({
+  selector: 'app-experts',
+  templateUrl: './experts.component.html',
+  styleUrls: ['./experts.component.scss']
+})
+export class ExpertsComponent implements OnInit {
+
+  experts: Observable<Expert[]>;
+
+  constructor(
+    private router: Router,
+    private expertService: ExpertService
+  ) { }
+
+  ngOnInit(): void {
+    this.experts = this.expertService.get();
+  }
+
+  showExpertDetails(id): void {
+    this.router.navigateByUrl(`/experts/${id}`);
+  }
+
+}
