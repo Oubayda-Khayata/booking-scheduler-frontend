d28dddf54c77c4af209548863e2e7a69f76ee55b
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-08-06 19:26:40 +0300

    Update expert details when timezone has been changed

--- src/app/components/expert-details/expert-details.component.ts
+++ src/app/components/expert-details/expert-details.component.ts
@@ -1,29 +1,39 @@
+import { TimezoneService } from './../../core/services/timezone/timezone.service';
 import { ExpertService } from './../../shared/services/expert/expert.service';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import { Expert } from './../../shared/models/expert';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Expert } from './../../shared/models/expert.model';
 
 @Component({
   selector: 'app-expert-details',
   templateUrl: './expert-details.component.html',
-  styleUrls: ['./expert-details.component.scss']
+  styleUrls: ['./expert-details.component.scss'],
 })
 export class ExpertDetailsComponent implements OnInit {
-
-  @Input() expert: Expert;
+  expert: Expert;
   get imageUrl(): string {
     return this.expert ? `./assets/images/${this.expert.gender}.png` : '';
   }
+  id: number;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private expertService: ExpertService) { }
+    private expertService: ExpertService,
+    private timezoneService: TimezoneService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = +params.get('id');
-      this.expertService.getExpert(id).subscribe(expert => this.expert = expert);
-    })
+    this.route.paramMap.subscribe((params) => {
+      this.id = +params.get('id');
+      this.getExpert();
+    });
+    this.timezoneService.currentTimezoneState.subscribe((timezone: string) => this.getExpert());
+  }
+
+  getExpert(): void {
+    this.expertService
+      .getExpert(this.id, this.timezoneService.currentTimezone)
+      .subscribe((expert: Expert) => this.expert = expert);
   }
 
   bookNow(): void {
@@ -31,5 +41,4 @@ export class ExpertDetailsComponent implements OnInit {
       this.router.navigateByUrl(`/appointment/${this.expert.id}`);
     }
   }
-
 }
